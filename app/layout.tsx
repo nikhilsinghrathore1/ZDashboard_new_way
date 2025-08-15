bdd18e018154870b4006fac91628d16396184867
Mohit <mohit11994cu@gmail.com>
2025-08-15 16:33:55 +0530

    Wallet Connected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./Providers"; // <-- IMPORT IT
 import SideNavbar from "./components/SideNavbar";
 import TopNavbar from "./components/TopNavbar";
 
@@ -29,15 +30,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        <div className="w-full h-screen overflow-hidden bg-[#020817] flex">
-          <SideNavbar />
+        {/* The Provider component must wrap your entire layout */}
+        <Providers> {/* <-- WRAP EVERYTHING HERE */}
+          <div className="w-full h-screen overflow-hidden bg-[#020817] flex">
+            <SideNavbar />
 
-          <div className="w-[83.35%] h-full relative overflow-y-auto ">
-            <TopNavbar />
-                    
-            {children}
+            <div className="w-[83.35%] h-full relative overflow-y-auto ">
+              <TopNavbar />
+                      
+              {children}
+            </div>
           </div>
-        </div>
+        </Providers> {/* <-- END WRAPPER HERE */}
       </body>
     </html>
   );
