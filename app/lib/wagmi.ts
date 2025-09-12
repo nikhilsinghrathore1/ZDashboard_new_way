5c13db7f1fe3924cc63ec71641c07763ccf43d3a
Mohit <157012303+iammohit64@users.noreply.github.com>
2025-09-12 22:42:24 +0530

    Update wagmi.ts

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -7,7 +7,7 @@ import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 const projectId = '81bd965ef6722331a6c12ed6611a18a4' // <-- PASTE YOURS HERE
 const alchemyAmoyRpcUrl = 'https://polygon-amoy.g.alchemy.com/v2/vaNXsjVhxRKHbxsDADJfz';
 // Make sure you add the new variable to your Vercel settings
-const alchemyBaseSepoliaRpcUrl = process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC_URL;
+const alchemyBaseSepoliaRpcUrl = 'https://base-sepolia.g.alchemy.com/v2/U7Oi3BUH9b1uvMknFkyqp';
 
 
 // 
